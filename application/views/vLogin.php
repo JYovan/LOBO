074b137ffc09fc4e2cf6d6bb7ad28104f0cdefc1
JYovan <metalrock3@hotmail.es>
2018-03-09 00:32:41 -0600

    ASSETS

---
+++ application/views/vLogin.php
@@ -0,0 +1,13 @@
+<!--vLogin-->
+<body class="text-center">
+    <form class="form-signin">
+        <img class="mb-4" src="<?php print base_url('img/LS.png'); ?>" alt="" width="72" height="72">
+        <h1 class="h3 mb-3 font-weight-normal">ACCESO LOBO</h1>
+        <label for="inputEmail" class="sr-only">Usuario</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Usuario" required autofocus>
+        <label for="inputPassword" class="sr-only">Contraseña</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">ACCEDER</button>
+        <p class="mt-5 mb-3 text-muted">&copy; 2018</p>
+    </form>
+</body>
