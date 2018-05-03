cb24dffbb6d824b8907dfb09f9b6d2f61f923f28
JYovan <metalrock3@hotmail.es>
2018-05-03 13:16:38 -0500

    PEDIDO | ESTRUCTURA
    http://gregfranko.com/jquery-best-practices/#/25

---
+++ application/controllers/Pedido.php
@@ -0,0 +1,34 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pedido extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        date_default_timezone_set('America/Mexico_City');
+        $this->load->library('session');
+        $this->load->model('pedido_model'); 
+    }
+
+    public function index() {
+        if (session_status() === 2 && isset($_SESSION["LOGGED"])) {
+            $this->load->view('vEncabezado');
+            $this->load->view('vNavegacion');
+            $this->load->view('vPedido');
+            $this->load->view('vFooter');
+        } else {
+            $this->load->view('vEncabezado');
+            $this->load->view('vSesion');
+            $this->load->view('vFooter');
+        }
+    }
+
+    public function getRecords() {
+        try {
+            print json_encode($this->pedido_model->getRecords());
+        } catch (Exception $exc) {
+            echo $exc->getTraceAsString();
+        }
+    }
+}
---
+++ application/views/vPedido.php
@@ -0,0 +1,76 @@
+<div class="card " id="pnlTablero">
+    <div class="card-body">
+        <div class="row">
+            <div class="col-sm-6 float-left">
+                <legend class="float-left">Gestión de Pedidos</legend>
+            </div>
+            <div class="col-sm-6 float-right" align="right">
+                <button type="button" class="btn btn-primary" id="btnNuevo"><span class="fa fa-plus"></span><br></button>
+                <button type="button" class="btn btn-primary" id="btnConfirmarEliminar"><span class="fa fa-trash"></span><br></button>
+            </div>
+        </div>
+        <div class="card-block">
+            <div class="table-responsive" id="tblRegistros"></div>
+        </div>
+    </div>
+</div>
+
+<!--NUEVO PEDIDO-->
+<div class="modal" id="mdlNuevo" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">NUEVO PEDIDO</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+
+            </div>
+            <div class="modal-footer"> 
+                <button type="button" class="btn btn-raised btn-primary" id="btnGuardar">GUARDAR</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!--ELIMINAR PEDIDO-->
+<div class="modal" id="mdlConfirmar" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Confirmar</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Deseas eliminar el registro?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">CANCELAR</button>
+                <button type="button" class="btn btn-raised btn-primary" id="btnEliminar">ACEPTAR</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--SCRIPT-->
+<script>
+    var master_url = base_url +'index.php/Pedido';
+    // IIFE - Immediately Invoked Function Expression
+    (function(yourcode) {
+
+    // The global jQuery object is passed as a parameter
+    yourcode(window.jQuery, window, document);
+    }(function($, window, document) {
+
+    // The $ is now locally scoped 
+
+    // Listen for the jQuery ready event on the document
+    $(function() {
+
+    });
+</script>
