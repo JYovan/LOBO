0568c8efc0bcf6faf42a424892ef25c73e7a4c3b
JYovan <metalrock3@hotmail.es>
2018-05-23 14:13:49 -0500

    CONEXION A MYSQL(SISTEMA LOBO), SQL SERVER (MAGNUS)
    db = Es el de SQL SERVER
    dbi = Es de MYSQL, pero es necesario cargarlo en cada modelo

--- application/models/materiales_model.php
+++ application/models/materiales_model.php
@@ -8,6 +8,7 @@ class materiales_model extends CI_Model {
 
     public function __construct() {
         parent::__construct();
+        $this->dbi = $this->load->database('dbi', true);
     }
 
     public function getRecords() {
