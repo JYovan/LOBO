aa41e1cf07d80eeb648e7df0ee31f31a32919fae
ChristianMMJ <christian.m.m91@gmail.com>
2018-06-18 09:04:00 -0500

    Funciones de fecha

--- js/scripts.js
+++ js/scripts.js
@@ -255,6 +255,49 @@ var tableOptionsMiniTables = {
             //    ]
 };
 
+function getToday() {
+    var date = new Date();
+
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+
+    if (month < 10)
+        month = "0" + month;
+    if (day < 10)
+        day = "0" + day;
+
+    var today = day + "-" + month + "-" + year;
+    return today;
+}
+
+function getTodayWithTime() {
+    var date = new Date();
+
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+
+    if (month < 10)
+        month = "0" + month;
+    if (day < 10)
+        day = "0" + day;
+
+    var today = day + "-" + month + "-" + year;
+
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    var strTime = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
+
+    return today + ' ' + strTime;
+}
+
 function getTable(tblname, data) {
     var column = '';
     var i = 0;
